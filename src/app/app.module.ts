ac9d6a74190bcd02f9714837278e9cd0fcaa0c1c
josepereza <josepereza66@gmail.com>
2022-11-02 17:42:12 +0100

    first commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {LayoutModule} from '@angular/cdk/layout'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
